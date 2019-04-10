b11227ef67315b1950236ca391dbf140a038da43
Vitalie Andries <vitalie.andries@gmail.com>
2019-04-10 19:20:51 +0300

    test(logger):

--- src/loggers/loggers.spec.ts
+++ src/loggers/loggers.spec.ts
@@ -1,4 +1,5 @@
-import { ConsoleLogger, ErrTrackerLogger } from './loggers';
+import { ConsoleLogger, DummyLogger, ErrTrackerLogger, getLogger } from './loggers';
+import { ErrTrackerConfig } from '../core/ETErrorEvent';
 
 describe('Loggers', () => {
   let logger: ErrTrackerLogger;
@@ -20,5 +21,24 @@ describe('Loggers', () => {
     expect(console.log).toHaveBeenCalledWith('ErrTracker:ok', 'zzz');
     expect(console.warn).toHaveBeenCalledWith('ErrTracker:fail', 'qqq');
     // logger.success('zzz');
+  });
+
+  it('should have success and error methods defined', function() {
+    expect(DummyLogger.success).toBeDefined();
+    expect(DummyLogger.error).toBeDefined();
+    const trySuccess = (msg: any) => DummyLogger.success(msg);
+    const tryError = (msg: any) => DummyLogger.error(msg);
+
+    expect(() => trySuccess('test')).not.toThrow();
+    expect(() => tryError('test')).not.toThrow();
+  });
+
+  it('should provide logger based in config', () => {
+    const consoleLogger = getLogger({ useConsoleLogger: true });
+
+    expect(consoleLogger).toEqual(ConsoleLogger);
+    const defaultLogger = getLogger({} as ErrTrackerConfig);
+
+    expect(defaultLogger).toEqual(DummyLogger);
   });
 });
